ed251b625716fa318894032d2c2202c11f3e0401
Sarang Mohaniraj <42594517+SarangMohaniraj@users.noreply.github.com>
2019-10-28 12:57:09 -0400

    Update Data Pre-processing

--- train.py
+++ train.py
@@ -15,6 +15,9 @@ with open("data/UMich.csv") as file:
 	file.close()
 
 
+gpa = []
+acts = []
+
 with open("concordance.csv") as file:
 	concordance = list(csv.reader(file))
 	for element in dataset:
@@ -24,7 +27,17 @@ with open("concordance.csv") as file:
 		# convert SAT to ACT and choose the higher score
 		concordance_sat = int(concordance[int((1600-int(sat))/10)][1]) if sat != "-" else None
 		act = concordance_sat if act == "-" or (concordance_sat != None and act != "-" and concordance_sat > int(act)) else int(act)
-		element[features.index("ACT")] = act
+		element[features.index("ACT")] = act/36 #normalize act
+
+		####### IF CONVERTING TO NUMPY FLOAT32 THEN DELETE THIS ######START HERE########
+		#convert GPA and year to number, normalize gpa
+		element[features.index("GPA")] = float(element[features.index("GPA")])/5 #normalize GPA
+		element[features.index("Year")] = int(element[features.index("Year")])
+		####### IF CONVERTING TO NUMPY FLOAT32 THEN DELETE THIS ######END HERE########
+
+		##this is only to show the distribition, so once you are good you can delete all instances of these lists
+		gpa.append(element[features.index("GPA")])
+		acts.append(element[features.index("ACT")])
 
 		#delete SAT column because all scores have been converted to ACT
 		del(element[features.index("Highest Comb SAT 1600")])
@@ -32,22 +45,13 @@ with open("concordance.csv") as file:
 	features.remove("Highest Comb SAT 1600")
 	file.close()
 
-
-gpa = []
-acts = []
-####### IF CONVERTING TO NUMPY FLOAT32 THEN DELETE THIS ########
-for element in dataset:
-	#convert GPA and year to number
-	element[features.index("GPA")] = float(element[features.index("GPA")])
-	element[features.index("Year")] = int(element[features.index("Year")])
-
-	gpa.append(element[features.index("GPA")])
-	acts.append(element[features.index("ACT")])
-
-plt.subplot(1,2,1)
+plt.figure(figsize=(12,4))
+plt.subplot(1,3,1)
 plt.hist(gpa)
-plt.subplot(1,2,2)
+plt.subplot(1,3,2)
 plt.hist(acts)
+plt.subplot(1,3,3)
+plt.scatter([i * 5 for i in gpa],[i * 36 for i in acts],marker='X')
 plt.show()
 
 
